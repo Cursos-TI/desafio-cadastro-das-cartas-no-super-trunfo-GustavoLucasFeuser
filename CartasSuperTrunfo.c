34f8fa459b7af8d78138a88da506989d9ffd7bdd
Gustavo Lucas Feuser <125416702+GustavoLucasFeuser@users.noreply.github.com>
2025-02-05 14:45:30 +0000

    printsf e scansf feitos e funcionando

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,9 @@
 //Teste larissa
 
 int main() {
+    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
     //declaração das variáveis
     float codecidade;
     char nomecidade[50];
@@ -15,13 +18,31 @@ int main() {
     float PIB;
     int numpontosturisticos;
 
-
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+
+    // solicitação de entrada de dados pelo usuário
+    printf("Digite o código da cidade: \n");
+    scanf("%f", &codecidade);
+
+    getchar();
+
+    printf("Digite o nome da cidade: \n");
+    fgets(nomecidade, sizeof(nomecidade), stdin);
+
+    printf("Digite a população da cidade: \n");
+    scanf("%d", &populacao);
+
+    printf("Digite a área da cidade: \n");
+    scanf("%f", &area);
+
+    printf("Digite o PIB da cidade: \n");
+    scanf("%f", &PIB);
+
+    printf("Digite o número de pontos turisticos da cidade: \n");
+    scanf("%d", &numpontosturisticos);
+
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
